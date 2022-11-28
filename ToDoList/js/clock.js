0e52dfa05f650b3ed75963843bcd62a55dbc9add
JDG <jang75967@gmail.com>
2022-11-28 19:52:22 +0900

    ToDoList 파일 추가

---
+++ ToDoList/js/clock.js
@@ -0,0 +1,19 @@
+// whole-script strict mode syntax
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+'use strict';
+
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
